ef9e03815ebf37219089a7d28d412fde671df843
Thomas Berry <thomasdpb@gmail.com>
2019-05-13 02:06:20 +0100

    lidar plot

--- get_lidar_plot.m
+++ get_lidar_plot.m
@@ -3,11 +3,15 @@ function plt = get_lidar_plot(lidar)
     scan = LidarScan(lidar);
     
     % 10000 will be our choice for 
-    scan(scan==0) = 10000;
+    scan(scan==0) = 7000;
     scan = scan / 1000;
     
-    plt(1,:) = linspace(-2*pi/3,2*pi/3,682);
-    plt(2,:) = fliplr(scan);
+    theta_resol = 1;
     
+    
+    plt(1,:) = linspace(-2*pi/3,2*pi/3,360/theta_resol);
+    
+    %plt(2,:) = fliplr(scan);
+    plt(2,:) = scan;
 
 end
